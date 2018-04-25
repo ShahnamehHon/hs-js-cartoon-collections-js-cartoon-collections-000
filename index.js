1d7ca493dffbf86abf4239ba286c622b9a8a592d
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-25 23:48:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
+
 function dwarfRollCall(dwarves) {
+  var dwarfString = "";
+  for (let i = 0; i < dwarves.length; i++) {
+    dwarfString += (i+1).toString() + ". " + dwarves[i] + " ";
+     }
+     return dwarfString;
+ 
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var planetCalls = [];
+  for(let i = 0; i<planeteerCalls.length; i++) {
+    planetCalls.push(planeteerCalls[i].toUpperCase + '!');
+  }
+  return planetCalls;
 }
 
 function longPlaneteerCalls(words) {
-}
+  for(let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    } else {
+      return false; 
+    }
+    }
+  }
+
 
 function findTheCheese (foods) {
-}
+  var cheese = ["cheddar", "gouda", "camembert"];
+  for (var i = 0; i<foods.length; i++) {
+  for (var j = 0; j<cheese.length; j++){
+    if (foods[i] === cheese[j]){
+      return foods[i];
+    }
+    }
+  }
+ return "no cheese!";
+ }
+
